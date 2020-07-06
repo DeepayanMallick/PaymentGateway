71db08e59cd25d09d3c04684420c232796cd8d8f
DeepayanMallick <deepayan.cse@gmail.com>
2020-07-06 17:43:56 +0600

    small update

--- app/Billing/PaymentGateway.php
+++ app/Billing/PaymentGateway.php
@@ -3,8 +3,16 @@
 
 namespace App\Billing;
 
-
 class PaymentGateway
 {
-
+    public function setPayment()
+    {
+        if (request()->has('paypal')) {
+            return new PaypalPaymentGateway('usd');
+        } elseif (request()->has('credit')) {
+            return new CreditPaymentGateway('usd');
+        } else {
+            return new StripePaymentGateway('usd');
+        }
+    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Billing\CreditPaymentGateway;
-use App\Billing\PaypalPaymentGateway;
-use App\Billing\StripePaymentGateway;
+use App\Billing\PaymentGateway;
 use App\Interfaces\PaymentGatewayInterface;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,13 +12,8 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(PaymentGatewayInterface::class, function ($app) {
-            if(request()->has('paypal')){
-                return new PaypalPaymentGateway('usd');
-            } elseif (request()->has('credit')){
-                return new CreditPaymentGateway('usd');
-            } else {
-                return new StripePaymentGateway('usd');
-            }
+            $paymentGateway =  new PaymentGateway();
+            return $paymentGateway->setPayment();
         });
     }
 
